e6fbd2a7ce0dfff74bad20188232ed0d292b5608
0vector0 <0vector0@gmail.com>
2016-10-29 15:43:56 +0300

    Initial commit

---
+++ src/main/java/com/mykhalechko/CalculatorController.java
@@ -0,0 +1,20 @@
+package com.mykhalechko;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+import java.io.IOException;
+
+@Controller
+public class CalculatorController {
+
+    @MessageMapping("/addNum")
+    public String addNumbers(String input) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        CalculatorInput data = mapper.readValue(input, CalculatorInput.class);
+        CalculatorResult result = new CalculatorResult();
+        result.setResult("" + (data.getNum1() + data.getNum2()));
+        return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(result);
+    }
+}
